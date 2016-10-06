c5663a21d8acb024d947f118f26e544739c3a833
celestial964 <ivangricak@MacBook-Pro-Ivan.local>
2016-10-06 22:47:31 +0500

    RandomCommitMessage

--- js/GLscripts.js
+++ js/GLscripts.js
@@ -1,7 +1,7 @@
 var editGrpLst, editInGrpLst;
-$('#editGrpLst1').hover(function(){$('#editGrpList1').prepend("<h1>SMTH</h1>")});
-$('#editGrpLst2').hover(function(){$('#editGrpList2').prepend("<h1>SMTH</h1>")});
-$('#editGrpLst3').hover(function(){$('#editGrpList3').prepend("<h1>SMTH</h1>")});
-$('#editGrpLst4').hover(function(){$('#editGrpList4').prepend("<h1>SMTH</h1>")});
-$('#editGrpLst5').hover(function(){$('#editGrpList5').prepend("<h1>SMTH</h1>")});
-$('#editGrpLst6').hover(function(){$('#editGrpList6').prepend("<h1>SMTH</h1>")});
+$('#editGrpLst1').click(function(){$('#textcenter').prepend('<h1 id="del1stCrsGrp">SMTH</h1>')});
+$('#editGrpLst2').click(function(){$('#editGrpList2').prepend('<h1 id="del2ndCrsGrp">SMTH</h1>')});
+$('#editGrpLst3').click(function(){$('#editGrpList3').prepend('<h1 id="del3rdCrsGrp">SMTH</h1>')});
+$('#editGrpLst4').click(function(){$('#editGrpList4').prepend('<h1 id="del4thCrsGrp">SMTH</h1>')});
+$('#editGrpLst5').click(function(){$('#editGrpList5').prepend('<h1 id="del5thCrsGrp">SMTH</h1>')});
+$('#editGrpLst6').click(function(){$('#editGrpList6').prepend('<h1 id="del6thtCrsGrp">SMTH</h1>')});
