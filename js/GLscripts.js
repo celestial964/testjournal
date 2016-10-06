3237de5d3c6f17ff0b7bea0496da56e7f62c9ddd
celestial964 <ivangricak@MacBook-Pro-Ivan.local>
2016-10-06 21:50:59 +0500

    js

--- js/GLscripts.js
+++ js/GLscripts.js
@@ -1,2 +1,7 @@
 var editGrpLst, editInGrpLst;
-$('#editGrpLst1').hover(function(){$('document').append("<h1>SMTH</h1>")});
+$('#editGrpLst1').hover(function(){$('#editGrpList1').prepend("<h1>SMTH</h1>")});
+$('#editGrpLst2').hover(function(){$('#editGrpList2').prepend("<h1>SMTH</h1>")});
+$('#editGrpLst3').hover(function(){$('#editGrpList3').prepend("<h1>SMTH</h1>")});
+$('#editGrpLst4').hover(function(){$('#editGrpList4').prepend("<h1>SMTH</h1>")});
+$('#editGrpLst5').hover(function(){$('#editGrpList5').prepend("<h1>SMTH</h1>")});
+$('#editGrpLst6').hover(function(){$('#editGrpList6').prepend("<h1>SMTH</h1>")});
