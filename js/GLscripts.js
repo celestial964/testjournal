dd5887921c8a93ced0077c4578189c23a4c5918a
celestial964 <ivangricak@MacBook-Pro-Ivan.local>
2016-10-13 20:55:49 +0500

    common

--- js/GLscripts.js
+++ js/GLscripts.js
@@ -1,5 +1,5 @@
 var editGrpLst, editInGrpLst;
-$('#editGrpLst1').click(function(){$('#textcenter').prepend('<h1 id="del1stCrsGrp">SMTH</h1>')});
+jquery('#editGrpLst1').click(function(){jquery('#textcenter').alert('SMTH')});
 $('#editGrpLst2').click(function(){$('#editGrpList2').prepend('<h1 id="del2ndCrsGrp">SMTH</h1>')});
 $('#editGrpLst3').click(function(){$('#editGrpList3').prepend('<h1 id="del3rdCrsGrp">SMTH</h1>')});
 $('#editGrpLst4').click(function(){$('#editGrpList4').prepend('<h1 id="del4thCrsGrp">SMTH</h1>')});
